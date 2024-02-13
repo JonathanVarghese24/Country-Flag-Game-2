bad80cfb4625c29b6deaed903b506d805ea65d75
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-02-13 10:42:47 -0600

    Initial Commit

---
+++ Country Flag Game 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country Flag Game 2
+//
+//  Created by Jonathan Varghese on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Country Flag Game 2/Country_Flag_Game_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_Game_2App.swift
+//  Country Flag Game 2
+//
+//  Created by Jonathan Varghese on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_Game_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
